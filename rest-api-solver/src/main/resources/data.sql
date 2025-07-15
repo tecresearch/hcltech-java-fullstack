0d2d7a63855ef6c0261505148c19dd00332b469b
tecresearch <bnlv1212@gmail.com>
2025-07-15 21:50:53 +0530

    Project:Rest-API-Solver:H2-Database,Data-JPA Configuration have been successfully completed

---
+++ rest-api-solver/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO employee(name, email, department, join_date, salary)
+VALUES 
+('John Doe', 'john@example.com', 'HR', '2020-01-15', 50000.0),
+('Jane Smith', 'jane@example.com', 'Engineering', '2021-03-22', 75000.0);
