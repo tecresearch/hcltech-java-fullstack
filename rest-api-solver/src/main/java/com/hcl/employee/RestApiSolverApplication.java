6e95a9344736f624ad8cf1dc294cbcef8deff4e3
tecresearch <bnlv1212@gmail.com>
2025-07-24 18:16:54 +0530

    from personal pc updates

---
+++ rest-api-solver/src/main/java/com/hcl/employee/RestApiSolverApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSolverApplication.class, args);
+	}
+
+}
